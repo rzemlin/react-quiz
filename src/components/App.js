e9547043e838ba1ca8f19bb220419379c319c355
Ryan Zemlin <rzemlin@gmail.com>
2023-12-06 15:56:42 -0800

    add fake api to test

--- src/components/App.js
+++ src/components/App.js
@@ -10,8 +10,27 @@ import FinishScreen from "./FinishScreen";
 import Footer from "./Footer";
 import Timer from "./Timer";
 import { useQuiz } from "../contexts/QuizContext";
+import { useEffect, useReducer } from "react";
 
 export default function App() {
+  const initialState = {
+    questions: [],
+    status: "loading",
+  };
+  function reducer(state, action) {
+    switch (action.type) {
+      case "data received":
+    }
+  }
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.log("ERROR"));
+  }, []);
+
   const { status } = useQuiz();
 
   return (
